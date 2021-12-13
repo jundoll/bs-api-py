9446a9620982c25218dbd7651bd9fd2703473f47
jundoll <jundoll33@gmail.com>
2021-12-14 00:06:00 +0900

    change path

--- scoresaber/api/api.py
+++ scoresaber/api/api.py
@@ -1,13 +1,7 @@
 
-# change base directory
-import sys
-import pathlib
-current_dir = pathlib.Path(__file__).resolve().parent
-sys.path.append( str(current_dir) + '/../../' )
-
 # load modules
 from lib import api
-from scoresaber.entity import (Difficulty, LeaderboardInfo,
+from ..entity import (Difficulty, LeaderboardInfo,
                                LeaderboardInfoCollection, Player,
                                PlayerCollection, PlayerScoreCollection,
                                ScoreCollection)
