e8d7f5fcbe03589e97ddc04b337c691f4333d60b
jundoll <jundoll33@gmail.com>
2021-12-13 21:29:46 +0900

    test

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    name='bs-scoresaber-api',
+    name='bs_scoresaber_api',
     version="1.0.0",
-    license='MIT'
+    license='MIT',
+    packages=['scoresaber_api'],#, 'scoresaber_api.entity'],
+    package_dir={'scoresaber_api': 'scoresaber/api'}#,'scoresaber_api.entity': 'scoresaber/entity'}
 )
+
