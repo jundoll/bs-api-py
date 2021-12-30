6b81a2ad0f0156a010eab165e27875a91a84969e
jundoll <jundoll33@gmail.com>
2021-12-31 01:55:44 +0900

    test

--- setup.py
+++ setup.py
@@ -4,6 +4,13 @@ setup(
     name='BSAPI',
     version="0.1.0",
     license='MIT',
-    packages=['BSAPI', 'BSAPI.scoresaber'],
-    package_dir={'BSAPI': '', 'BSAPI.scoresaber': 'scoresaber/api'}
+    packages=[
+        'BSAPI.lib',
+        'BSAPI.scoresaber', 'BSAPI.scoresaber.entity'
+    ],
+    package_dir={
+        'BSAPI.lib': 'lib',
+        'BSAPI.scoresaber': 'scoresaber/api',
+        'BSAPI.scoresaber.entity': 'scoresaber/entity'
+    }
 )
