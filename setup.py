35703183d7630a2d86b9c486e1b0ebfed971363a
jundoll <jundoll33@gmail.com>
2022-01-03 00:11:52 +0900

    v0.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='BSAPI',
-    version="0.2.0",
+    version="0.2.1",
     license='MIT',
     packages=[
         'BSAPI.accsaber', 'BSAPI.accsaber.api', 'BSAPI.accsaber.entity',
