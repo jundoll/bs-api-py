9d4190324d39e039b26cdc83d4b2c44192f8cc2c
jundoll <jundoll33@gmail.com>
2023-04-09 18:46:07 +0900

    test

--- beatleader/api/leaderboard.py
+++ beatleader/api/leaderboard.py
@@ -21,20 +21,20 @@ async def get_leaderboards(
     # Default value : 10
     count: int = 10,
     #
-    sortBy: str = '',
+    sortBy: str = 'timestamp',
     #
-    order: str = '',
+    order: str = 'desc',
     #
     search: str = '',
     #
     type: str = '',
     #
     mode: str = '',
-    #
+    # category
     mapType: int = 0,
     # Available values : 0, 1, 2
     allTypes: int = 0,
-    # Available values : 0, 2, 4, 8, 16, 32
+    # feature(Available values : 0, 2, 4, 8, 16, 32)
     mapRequirements: int = 0,
     # Available values : 0, 1, 2
     allRequirements: int = 0,
@@ -81,29 +81,29 @@ async def get_leaderboards(
         query_list.append(f'type={type}')
     if mode:
         query_list.append(f'mode={mode}')
-    if mapType >= 0:
+    if mapType > 0:
         query_list.append(f'mapType={mapType}')
     if allTypes in [0, 1, 2]:
         query_list.append(f'allTypes={allTypes}')
-    if mapRequirements in [0, 2, 4, 8, 16, 32]:
+    if mapRequirements in [2, 4, 8, 16, 32]:
         query_list.append(f'mapRequirements={mapRequirements}')
     if allRequirements in [0, 1, 2]:
         query_list.append(f'allRequirements={allRequirements}')
     if mytype:
         query_list.append(f'mytype={mytype}')
-    if (stars_from >= 0) and (stars_from <= 18):
+    if (stars_from > 0) and (stars_from <= 18):
         query_list.append(f'stars_from={stars_from}')
     if (stars_from <= stars_to) and (stars_to > 0) and (stars_to <= 18):
         query_list.append(f'stars_to={stars_to}')
-    if (accrating_from >= 0) and (accrating_from <= 18):
+    if (accrating_from > 0) and (accrating_from <= 18):
         query_list.append(f'accrating_from={accrating_from}')
     if (accrating_from <= accrating_to) and (accrating_to > 0) and (techrating_to <= 18):
         query_list.append(f'accrating_to={accrating_to}')
-    if (passrating_from >= 0) and (passrating_from <= 18):
+    if (passrating_from > 0) and (passrating_from <= 18):
         query_list.append(f'passrating_from={passrating_from}')
     if (passrating_from <= passrating_to) and (passrating_to > 0) and (techrating_to <= 18):
         query_list.append(f'passrating_to={passrating_to}')
-    if (techrating_from >= 0) and (techrating_from <= 18):
+    if (techrating_from > 0) and (techrating_from <= 18):
         query_list.append(f'techrating_from={techrating_from}')
     if (techrating_from <= techrating_to) and (techrating_to > 0) and (techrating_to <= 18):
         query_list.append(f'techrating_to={techrating_to}')
