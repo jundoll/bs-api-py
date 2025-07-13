f04174599c70be9983013745dbec719cd44f6056
jundoll <jundoll33@gmail.com>
2025-07-13 13:06:28 +0900

    change api url

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='BSAPI',
-    version="0.5.1",
+    version="0.5.2",
     license='MIT',
     packages=[
         'BSAPI.accsaber', 'BSAPI.accsaber.api', 'BSAPI.accsaber.entity',
